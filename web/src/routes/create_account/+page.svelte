54d32e91f7503386ccc0fbdf85793c760fc0990d
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-02-20 16:19:53 -0500

    form input bound to value

--- web/src/routes/create_account/+page.svelte
+++ web/src/routes/create_account/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
-    async function submitCreateAccount(event: Event) {}
+    async function submitCreateAccount(event: Event) {};
+
+    async function onsubmit() {
+        console.log($state.snapshot(name));
+        console.log("sarah");
+    }
+
+    let name = $state("");
+
+    $inspect(name);
 
 </script>
 
 <h2>Create account</h2>
 
-<form on:submit|preventDefault={submitCreateAccount} method="POST">
+<form {onsubmit}>
     <label for="friend_code">Friend Code:</label>
     <input type="text" name="friend code" id="friend_code"/><br />
     <label for="name">Name</label>
-    <input type="text" name="name" id="name"/><br />
+    <input type="text" name="name" id="name" bind:value={name}/><br />
     <label for="email">E-mail:</label>
     <input type="text" name="email" id="email"/><br />
     <label for="password">Password:</label>

1af335e825d51b5913a4cf481cb82db646c5ad3d
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-02-21 11:03:17 -0500

    update faker password to be long enough

--- backend/celeryapp/spec/factories/user.rb
+++ backend/celeryapp/spec/factories/user.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     username { Faker::Internet.username }
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(min_length: 12) }
   end
 end

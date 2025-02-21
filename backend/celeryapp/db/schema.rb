ba9fbfeed18f276187a7eb5c5b6e4fc236e691e2
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-02-21 11:17:55 -0500

    add user reference to table

--- backend/celeryapp/db/schema.rb
+++ backend/celeryapp/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_14_212537) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_21_161544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "recommendations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.text "description"
+    t.string "who_recommended"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recommendations_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -34,5 +44,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_14_212537) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "recommendations", "users"
   add_foreign_key "sessions", "users"
 end

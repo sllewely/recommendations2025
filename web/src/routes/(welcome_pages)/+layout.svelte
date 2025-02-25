51276800d0fa0bb101de8c309cd7e7b02c23be61
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-02-25 18:53:39 -0500

    header and footer styling

--- web/src/routes/(welcome_pages)/+layout.svelte
+++ web/src/routes/(welcome_pages)/+layout.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
     import Header from './Header.svelte';
+    import Footer from './Footer.svelte';
     import '../../app.css';
 
     let { children } = $props();
 </script>
 
-<div class="app">
+<div class="flex flex-col h-screen justify-between">
     <Header />
 
     <main>
         {@render children()}
     </main>
 
-    <footer>
-        <p>
-            recommendations by celery
-        </p>
-    </footer>
+    <Footer />
 </div>
 
 
--- web/src/routes/(welcome_pages)/Header.svelte
+++ web/src/routes/(welcome_pages)/Header.svelte
@@ -2,24 +2,21 @@
 	import { page } from '$app/state';
 </script>
 
-<header class="bg-white">
-
-	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-		<div class="flex lg:flex-1">
-			<a href="/">
-				<span class="sr-only">Img text for screen reader</span>
-			</a>
+<header class="bg-gray-800 text-white py-4 px-3">
+	<div class="container mx-auto flex items-center justify-between">
+		<div class="w-full  md:text-center md:mb-0 mb-8">
+			<p class="text-xs text-gray-400 md:text-sm">by celery</p>
 		</div>
-		<div class="hidden lg:flex lg:gap-x-12">
-			<div class="relative">
-				<a href="/" class="text-sm/6 font-semibold text-gray-900">Home</a>
-				<a href="/about" class="text-sm/6 font-semibold text-gray-900">About</a>
-			</div>
+		<div class="w-full md:text-center md:mb-0 mb-8">
+			<ul class="list-reset flex justify-center flex-wrap text-xs md:text-sm gap-3">
+				<li><a href="/" class="text-gray-400 hover:text-white">Home</a></li>
+				<li class="mx-4"><a href="/about" class="text-gray-400 hover:text-white">About</a></li>
+				<li><a href="/create_account" class="text-gray-400 hover:text-white">Sign up</a></li>
+			</ul>
 		</div>
-		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-			<a href="/sign_in" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+		<div class="w-full  md:text-center md:mb-0 mb-8 justify-end">
+			<a href="/sign_in" class="text-sm/6 font-semibold text-gray-400 hover:text-orange-400" >Log in <span aria-hidden="true">&rarr;</span></a>
 		</div>
-	</nav>
-
+	</div>
 </header>
 

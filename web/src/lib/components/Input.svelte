6eb76b4adae7f05919be68f94a7102159a8b8bec
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-03-07 13:54:29 -0500

    sign in works after input component update

--- web/src/lib/components/Input.svelte
+++ web/src/lib/components/Input.svelte
@@ -3,9 +3,10 @@
     let req = required ? 'required' : '';
 </script>
 <div class="mb-5">
-    <label for={name} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+    <label for={name} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}
     <input
             type={input_type}
+            name={name}
             id={name}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             value={value}
@@ -13,4 +14,5 @@
            {req}
             autocomplete="off"
     />
+    </label>
 </div>

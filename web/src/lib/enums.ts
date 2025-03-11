0149b9268cd63471cff775375ec61b38dc5203e5
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-03-11 18:43:46 -0400

    form has toggle button

---
+++ web/src/lib/enums.ts
@@ -0,0 +1,7 @@
+// Enums
+
+export enum RecommendationStatus {
+    Interested = 0,
+    Watching = 1,
+    Recommend = 2,
+}
--- web/src/routes/(app)/posts/create/+page.svelte
+++ web/src/routes/(app)/posts/create/+page.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
     import {enhance} from '$app/forms';
     import Input from "$lib/components/form/Input.svelte";
+    import InputCheckbox from "$lib/components/form/InputCheckbox.svelte";
     import FormButton from "$lib/components/form/FormButton.svelte";
     import H1 from '$lib/components/text/H1.svelte'
     import Link from '$lib/components/text/Link.svelte'
+    import ToggleButton from '$lib/components/form/ToggleButton.svelte'
     import Card from '$lib/components/Card.svelte';
+    import LinkButton from "$lib/components/text/LinkButton.svelte";
+
+    import {RecommendationStatus} from "$lib/enums";
 
     let {data, form} = $props();
 
     let creating = $state(false);
+    let status = $state(RecommendationStatus.Interested);
+
+    // TODO:
+    // List of types, one can be selected
+    // One selected: interested, watching, recommend
+    // who recommended appears if interested
+    // rating appears if recommend or watching
 
 
 </script>
 
 <div>
 
-<H1>Create a post</H1>
+<H1>Create a recommendation post</H1>
 
 {#if creating }
     <p>signing in...</p>
@@ -36,11 +48,19 @@
     >
 
         <div class="flex flex-col">
+            <InputCheckbox name="recommendation_only" label="Don't post publicly, just save for me (TODO: not implemented):" />
             <Input name="post_title" label="Post Title:" value={form?.title} />
             <Card>
                 <H1>Recommendation</H1>
-                <Input name="title" label="Title:" />
                 <Input name="type" label="Type:" placeholder="book, movie, other"/>
+                <div>
+                    <span onclick={() => status = RecommendationStatus.Interested}><ToggleButton color="yellow" selected={status === RecommendationStatus.Interested}>&#10133; Interested</ToggleButton></span>
+                    <span onclick={() => status = RecommendationStatus.Watching}><ToggleButton color="orange" selected={status === RecommendationStatus.Watching}>&#10133; Watching</ToggleButton></span>
+                    <span onclick={() => status = RecommendationStatus.Recommend}><ToggleButton color="blue" selected={status === RecommendationStatus.Recommend}>&#10133; Recommend</ToggleButton></span>
+                </div>
+
+                <Input name="title" label="Title:" />
+
                 <p>TODO: Should be text area</p>
                 <Input name="notes" label="Notes:" />
                 <Card>

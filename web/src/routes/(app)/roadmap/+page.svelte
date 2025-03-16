4329c51c590731d80d2794257e052893a62d4bb4
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-03-16 12:05:21 -0400

    update roadmap doc

--- web/src/routes/(app)/roadmap/+page.svelte
+++ web/src/routes/(app)/roadmap/+page.svelte
@@ -7,7 +7,7 @@
 <div>
 <H1>Roadmap</H1>
     <p>I'm just one unpaid developer :') but all of your feedback is appreciated</p>
-    <p><Link url="https://docs.google.com/document/d/1AgK534T8KpFDt5CUouzuQY8Eq1XPpF0EkMRq0xPP3to/edit?tab=t.0#heading=h.6ib6oacsnuxb">Project Overview doc with roadmap</Link></p>
+    <p><Link url="https://docs.google.com/document/d/1MSynsmQ5SopVxjUfuWTPf7NQqSBIOHkqdjHIX0b4680/edit?usp=sharing">Roadmap</Link></p>
     <p><Link url="https://docs.google.com/forms/d/e/1FAIpQLSd_eaVSWDhCnIDbLFhf3OHO2qtQvwoYifKL1KJoPi71fTjIUw/viewform?usp=sharing">Submit a feature request!</Link></p>
 
 </div>

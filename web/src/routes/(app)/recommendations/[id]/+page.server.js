b97700d88949b2a690b09fd279ac7cca779fed60
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-03-18 08:39:34 -0400

    create recommendation page

--- web/src/routes/(app)/recommendations/[id]/+page.server.js
+++ web/src/routes/(app)/recommendations/[id]/+page.server.js
@@ -1,13 +1,18 @@
-import {getUser} from "$lib/api_calls/users.svelte.js";
-import {getPostsForUser} from "$lib/api_calls/posts.svelte.js";
+
 import {getRecommendation} from "$lib/api_calls/recommendations.svelte.js";
+import {token} from "$lib/api_calls/auth.svelte";
+import { getUser } from '$lib/api_calls/users.svelte.js';
 
 export async function load({ params }) {
 
     let recommendation_id = params.id;
     let recommendation = await getRecommendation(recommendation_id);
+    let user = await getUser(recommendation.creator_id);
+    let my_user_id = token.my_user_id;
 
     return {
         recommendation: recommendation,
+        user: user,
+        my_user_id: my_user_id,
     }
 }
--- web/src/routes/(app)/recommendations/[id]/+page.svelte
+++ web/src/routes/(app)/recommendations/[id]/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
     import H1 from "$lib/components/text/H1.svelte";
+    import H2 from "$lib/components/text/H2.svelte";
+    import Card from "$lib/components/Card.svelte";
 
     let {data} = $props();
+    let user = data.user;
     let recommendation = data.recommendation;
+
+
 </script>
 
 <div>
+    <H2>{user.name}'s recommendation</H2>
+    <Card>
     <H1>{recommendation.title}</H1>
+    <p>{recommendation.media_type}</p>
+    <p>{recommendation.notes}</p>
+    <p>Who recommended? {recommendation.who_recommended}</p>
+    <p>Added: {recommendation.create_date_string}</p>
+    </Card>
 </div>

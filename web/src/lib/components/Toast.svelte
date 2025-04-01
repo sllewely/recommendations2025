560252931ea02a47bcb0832c4d7327603474ad64
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-01 10:08:44 -0400

    toast flies in and out

--- web/src/lib/components/Toast.svelte
+++ web/src/lib/components/Toast.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+    import {fly} from 'svelte/transition';
     import { toast_message} from "$lib/state/toast.svelte";
-    let show_toast =  $derived((toast_message.message === '') ? 'hidden' : 'visible');
+    let show_toast =  $derived((toast_message.message === '') ? false : true);
 
 
     $inspect(toast_message.message);
+    $inspect(show_toast)
 </script>
 
-<header class="bg-teal-200 text-black py-4 px-3 {show_toast}">
+{#if show_toast}
+<header class="bg-teal-200 text-black py-4 px-3" transition:fly={{x: 200, duration: 2000}}>
     <div class="container mx-auto flex items-center justify-between">
+        <div>
         <p>{toast_message.message}</p>
+        </div>
+        <div>
+            <button class="hover:text-orange-500 text-xl" on:click={() => {toast_message.message=''}}>x</button>
+        </div>
     </div>
 </header>
+{/if}
 

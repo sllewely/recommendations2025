7c4ae7239d9a1f8a00adc0420304a01463307cef
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-03 10:22:42 -0400

    about page

--- web/src/lib/state/current_user.svelte.ts
+++ web/src/lib/state/current_user.svelte.ts
@@ -5,4 +5,9 @@ export let current_user = $state({
     id: '',
 });
 
+// Debated if this should be a $derived state, but I think I may add handling for expired tokens in the future
+export function isSignedIn(): boolean {
+    return current_user.auth_token === '';
+}
+
 

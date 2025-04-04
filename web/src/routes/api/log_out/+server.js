900aa00b893e54ce79026428dcecac74e6a7ce66
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-04 11:52:34 -0400

    duplicate import, also its unused lol - redirect in logout api

--- web/src/routes/api/log_out/+server.js
+++ web/src/routes/api/log_out/+server.js
@@ -1,11 +1,8 @@
-import {json, redirect} from '@sveltejs/kit';
-import { redirect } from '@sveltejs/kit';
+import {json} from '@sveltejs/kit';
 
 // Log out the user
 export async function POST({request, cookies}) {
     cookies.set('jwt', '', { path: '/' });
     cookies.set('user_id', '', { path: '/' });
-    // This doesn't work:
-    // redirect(301, '/');
     return json({}, { status: 201})
 }

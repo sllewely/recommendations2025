99caeb35eb0721bed65de17e1dc833a1cd50ea93
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-14 11:13:29 -0400

    begin notification wip

--- web/src/routes/(app)/friends/+page.server.ts
+++ web/src/routes/(app)/friends/+page.server.ts
@@ -1,16 +1,15 @@
 import * as api from '$lib/api_calls/api.svelte';
 import {getRecommendation} from "$lib/api_calls/recommendations.svelte.js";
-import { getUser } from '$lib/api_calls/users.svelte.js';
+import {getUser} from '$lib/api_calls/users.svelte.js';
 
 
-export async function load({ cookies, params }) {
+export async function load({cookies, params}) {
 
     const jwt = cookies.get('jwt');
     const my_user_id = cookies.get('user_id');
 
     // TODO: error handling ... with errors?
     let user_res = await getUser(jwt, my_user_id);
-
 
     return {
         my_user: user_res,
--- web/src/routes/(app)/friends/+page.svelte
+++ web/src/routes/(app)/friends/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import {enhance} from '$app/forms';
 
-import H1 from "$lib/components/text/H1.svelte";
-import Card from "$lib/components/Card.svelte";
-import {newToast, toasts, ToastType} from "$lib/state/toast.svelte";
+    import H1 from "$lib/components/text/H1.svelte";
+    import Card from "$lib/components/Card.svelte";
+    import {newToast, toasts, ToastType} from "$lib/state/toast.svelte";
     import Input from "$lib/components/form/Input.svelte";
     import FormButton from "$lib/components/form/FormButton.svelte";
 
@@ -15,23 +15,22 @@ import {newToast, toasts, ToastType} from "$lib/state/toast.svelte";
 </script>
 
 <div>
-<H1>Friends!</H1>
-<Card>
-    <p>Presently you can add someone as a friend using a friend code.</p>
-    <p>Your friend code is: <span>{data.my_user.friend_code}</span></p>
+    <H1>Friends!</H1>
+    <Card>
+        <p>Presently you can add someone as a friend using a friend code.</p>
+        <p>Your friend code is: <span>{data.my_user.friend_code}</span></p>
 
-    {#if creating }
-        <p>updating...</p>
-    {/if}
-    <form
-            method="POST"
-            action="?/add_friend"
-            use:enhance={() => {
+        {#if creating }
+            <p>updating...</p>
+        {/if}
+        <form
+                method="POST"
+                action="?/add_friend"
+                use:enhance={() => {
                     creating = true;
                     return async ({update, result}) => {
                         await update();
                         creating = false;
-                        console.log('sarah beep')
                         let res = result.data;
                         if (res.success) {
                             toasts.toast = newToast("Success updating your rsvp");
@@ -41,14 +40,13 @@ import {newToast, toasts, ToastType} from "$lib/state/toast.svelte";
                     };
 
         }}
+        >
 
-    >
+            <Input name="friend_code" label="Your new friend's code:" value={form?.friend_code}/>
+            <FormButton>Add friend</FormButton>
 
-        <Input name="friend_code" label="Your new friend's code:" value={form?.friend_code}/>
-        <FormButton>Add friend</FormButton>
+        </form>
 
-    </form>
-
-</Card>
+    </Card>
 
 </div>

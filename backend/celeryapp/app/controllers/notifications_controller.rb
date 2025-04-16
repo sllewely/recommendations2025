a74a9ddf295028e4d1beb7bd1bb8d6387a3d97b2
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-16 15:19:58 -0400

    clear notif api

--- backend/celeryapp/app/controllers/notifications_controller.rb
+++ backend/celeryapp/app/controllers/notifications_controller.rb
@@ -4,4 +4,20 @@ class NotificationsController < ApplicationController
     render json: current_user.notifications, status: :ok
   end
 
+  def update
+    @notif = current_user.notifications.find_by(id: params[:id])
+    render json: { error: "notif not found" }, status: :not_found and return if @notif.nil?
+    if @notif.update(notif_params)
+      render json: @notif, status: :ok
+    else
+      render json: { error: @notif.errors }, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def notif_params
+    params.permit(:active)
+  end
+
 end

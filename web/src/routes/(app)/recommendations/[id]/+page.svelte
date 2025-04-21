33bbff28d5b3c9457babdfbf697de59675a41d3a
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-21 12:49:04 -0400

    bug fix to allow me to edit my own recommendation

--- web/src/routes/(app)/recommendations/[id]/+page.svelte
+++ web/src/routes/(app)/recommendations/[id]/+page.svelte
@@ -9,23 +9,26 @@
     let user = data.user;
     let recommendation = data.recommendation;
 
+    console.log(user);
+    console.log(data.my_user_id);
+
 
 </script>
 
 <div>
-    {#if data.my_user_id === user.id}
-    <div class="float-right relative">
-        <LinkButton url="/recommendations/{recommendation.id}/edit">Edit</LinkButton>
-        <LinkButton url="/recommendations/{recommendation.id}/delete">Delete</LinkButton>
+    {#if data.my_user_id.toString() === user.id.toString()}
+        <div class="float-right relative">
+            <LinkButton url="/recommendations/{recommendation.id}/edit">Edit</LinkButton>
+            <LinkButton url="/recommendations/{recommendation.id}/delete">Delete</LinkButton>
 
-    </div>
+        </div>
     {/if}
     <H2>{user.name}'s recommendation</H2>
     <Card>
-    <H1>{recommendation.title}</H1>
-    <p>{recommendation.media_type}</p>
-    <p>{recommendation.notes}</p>
-    <p>Who recommended? {recommendation.who_recommended}</p>
-    <p>Added: {recommendation.create_date_string}</p>
+        <H1>{recommendation.title}</H1>
+        <p>{recommendation.media_type}</p>
+        <p>{recommendation.notes}</p>
+        <p>Who recommended? {recommendation.who_recommended}</p>
+        <p>Added: {recommendation.create_date_string}</p>
     </Card>
 </div>

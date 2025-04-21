fe3e9df8d5521b28020bdd551687be871de24e0f
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-21 17:20:54 -0400

    idk table recommendations tuff broke everything

--- web/src/routes/(app)/recommendations/users/[user_id]/+page.svelte
+++ web/src/routes/(app)/recommendations/users/[user_id]/+page.svelte
@@ -9,6 +9,7 @@
     import {current_user} from '$lib/state/current_user.svelte.js';
 
     import * as Table from "$lib/components/ui/table";
+    import Link from "$lib/components/text/Link.svelte";
 
     let {data} = $props();
 
@@ -22,7 +23,16 @@
 
     <p>Save the things you love, track what you want to enjoy next, share interests with your friends</p>
 
-    <div>Create a new recommendation</div>
+    <div>
+        <Link url="/recommendations/create">Create a new recommendation</Link>
+    </div>
+    <hr/>
+
+    <div>
+
+    </div>
+
+    <hr/>
 
     <div>List of recommendations you've saved</div>
 
@@ -35,18 +45,22 @@
                     <Table.Head class="w-[100px]">Status</Table.Head>
                     <Table.Head>Type</Table.Head>
                     <Table.Head>Recommendation</Table.Head>
-                    <Table.Head class="text-right">more</Table.Head>
+                    <Table.Head class="w-[100px]">who recommended?</Table.Head>
+                    <Table.Head class="w-[100px] text-right">tags</Table.Head>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
                 {#each recommendations as rec}
 
                     <Table.Row>
+
                         <Table.Cell class="font-medium">{rec.status}</Table.Cell>
                         <Table.Cell>{rec.media_type}</Table.Cell>
                         <Table.Cell>{rec.title}</Table.Cell>
-                        <Table.Cell class="text-right">$250.00</Table.Cell>
+                        <Table.Cell class="text-right">{rec.who_recommended}</Table.Cell>
+                        <Table.Cell class="text-right">not implemented</Table.Cell>
                     </Table.Row>
+
                 {/each}
             </Table.Body>
         </Table.Root>

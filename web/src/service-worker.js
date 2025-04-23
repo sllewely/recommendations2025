877eecc0b0828c084d6df644e33f48927677527f
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-23 16:49:47 -0400

    service worker installing but import error

--- web/src/service-worker.js
+++ web/src/service-worker.js
@@ -1,3 +1,23 @@
 // Use npx pwa-asset-generator logo.png icons
 
 // import workbox from a cdn
+
+// This code executes in its own worker or thread
+self.addEventListener("install", event => {
+    console.log("Service worker installed");
+});
+self.addEventListener("activate", event => {
+    console.log("Service worker activated");
+});
+
+self.addEventListener('push', function (event) {
+    const data = event.data.json();  // Assuming the server sends JSON
+    const options = {
+        body: data.body,
+        icon: 'icon.png',
+        badge: 'badge.png'
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});

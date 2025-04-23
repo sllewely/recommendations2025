668e232ac0fdf525620a2453c03f03cdff48c644
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-23 16:49:47 -0400

    service worker installing but import error

---
+++ web/src/routes/(app)/notifications/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {Button} from "$lib/components/ui/button";
+
+    const requestNotifPermissions = () => {
+        Notification.requestPermission().then((result) => {
+            if (result === "granted") {
+                randomNotification();
+            }
+        });
+    };
+
+    function randomNotification() {
+        // const randomItem = Math.floor(Math.random() * games.length);
+        // const notifTitle = games[randomItem].name;
+        // const notifBody = `Created by ${games[randomItem].author}.`;
+        // const notifImg = `data/img/${games[randomItem].slug}.jpg`;
+        // const options = {
+        //     body: "My notif!!",
+        //     icon: notifImg,
+        // };
+        new Notification("My test notification!!");
+        setTimeout(randomNotification, 30000);
+    }
+
+
+</script>
+
+<Button onClick={requestNotifPermissions} className=" bg-lime-200 " id="notifications">*~Notif permission~*</Button>

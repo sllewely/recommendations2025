4af8f1c1bce108f4f7ca4c1d5ad9caecaa366b3d
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-23 11:05:40 -0400

    style and user link comments

--- web/src/lib/components/posts/Comment.svelte
+++ web/src/lib/components/posts/Comment.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
     import {friends_map} from "$lib/state/friends_map.svelte";
     import {rails_datetime_pretty} from "$lib/utils/dates.svelte";
+    import Link from '$lib/components/text/Link.svelte';
 
     const {comment} = $props();
     const author = $derived(friends_map.friends_map[comment.user_id] ?? {});
 </script>
 
 <div class="flex justify-left">
-    <article class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
-        <div class="flex justify-between items-center mb-2">
+    <article class="p-2 text-base bg-white rounded-lg dark:bg-gray-900">
+        <div class="flex justify-between items-center ">
             <div class="flex items-center">
                 <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                    {author.name}
+                    <Link url="/users/{comment.user_id}">{author.name}</Link>
                 </p>
                 <p class="text-sm text-gray-600 dark:text-gray-400">
                     {rails_datetime_pretty(comment.created_at)}

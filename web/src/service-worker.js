24d16c9643ac1e467b2e4efc5a31ecd92e558089
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-04-24 12:50:22 -0400

    insecure allow localtunnel csrf and origin host

--- web/src/service-worker.js
+++ web/src/service-worker.js
@@ -2,6 +2,12 @@
 
 // import workbox from a cdn
 
+/// <reference types="@sveltejs/kit" />
+import {build, files, version} from '$service-worker';
+
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
+
 // This code executes in its own worker or thread
 self.addEventListener("install", event => {
     console.log("Service worker installed");
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig, loadEnv} from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(),]
+
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [sveltekit(), tailwindcss(),],
+        server: {
+            allowedHosts: JSON.parse(env.VITE_ALLOWED_HOSTS),
+        },
+        csrf: {
+            checkOrigin: JSON.parse(env.VITE_CHECK_ORIGIN),
+        }
+    };
 });
+

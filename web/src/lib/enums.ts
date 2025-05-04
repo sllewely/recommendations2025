d9cb3819fb7bf0479ee292a2263c5245941ad1f6
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-05-04 15:00:31 -0400

    media type input buttons and recommendations forms improvements

--- web/src/lib/enums.ts
+++ web/src/lib/enums.ts
@@ -5,3 +5,13 @@ export enum RecommendationStatus {
     Watching = 1,
     Recommend = 2,
 }
+
+export enum MediaType {
+    Book = 0,
+    Movie = 1,
+    TvShow = 2,
+    Article = 3,
+    Music = 4,
+    Website = 5,
+
+}
--- web/src/routes/(app)/recommendations/create/+page.svelte
+++ web/src/routes/(app)/recommendations/create/+page.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
     import {enhance} from '$app/forms';
-    import Input from "$lib/components/form/Input.svelte";
+    import {Input} from "$lib/components/ui/input/index.js";
     import InputCheckbox from "$lib/components/form/InputCheckbox.svelte";
     import FormButton from "$lib/components/form/FormButton.svelte";
     import H1 from '$lib/components/text/H1.svelte'
-    import Link from '$lib/components/text/Link.svelte'
     import ToggleButton from '$lib/components/form/ToggleButton.svelte'
     import Card from '$lib/components/Card.svelte';
     import LinkButton from "$lib/components/text/LinkButton.svelte";
-    import * as RadioGroup from "$lib/components/ui/radio-group/index.js";
     import {Label} from "$lib/components/ui/label/index.js";
+    import {Button} from "$lib/components/ui/button/index.js";
+    import {Textarea} from "$lib/components/ui/textarea/index.js";
 
-    import {RecommendationStatus} from "$lib/enums";
+
+    import {MediaType, RecommendationStatus} from "$lib/enums";
     import {current_user} from "$lib/state/current_user.svelte";
     import {goto} from "$app/navigation";
     import {newToast, toasts, ToastType} from "$lib/state/toast.svelte";
+    import {Book, Clapperboard, Gamepad2, MonitorPlay, Link} from "@lucide/svelte";
 
     let {data, form} = $props();
 
     let creating = $state(false);
     let status = $state(RecommendationStatus.Interested);
+
+    // TODO: save & remember the user's last selection
+    let media_type = $state('book');
 
     // TODO:
     // List of types, one can be selected
@@ -32,14 +37,12 @@
 
 <div>
 
-    <H1>Create a recommendation post</H1>
-
-    <p>Hey btw not all of the fields are implemented.</p>
-    <p>Would love a volunteer to let me sit with them as they fill out recommendations & to hear your thoughts, so I can
-        figure out the UX</p>
+    <H1>Save a recommendation</H1>
+    <span class="text-cyan-800">Save your favorites, save what you want to remember for the future, or share what you're reading or watching
+        right now with your friends</span>
 
     {#if creating }
-        <p>signing in...</p>
+        <p>creating in...</p>
     {/if}
     <Card>
         <form
@@ -64,70 +67,57 @@
         }}
         >
 
-            <div class="flex flex-col">
-                <InputCheckbox name="recommendation_only"
-                               label="Don't post publicly, just save for me (TODO: not implemented):"/>
-                <Card>
-                    <Input name="title" label="Title:" value={form?.title}/>
-                    <H1>Recommendation</H1>
-                    <!--                <Input name="media_type" label="Type:" placeholder="book, movie, other" value={form?.media_type} list="mediaTypeList"/>-->
-                    <!--                <datalist id="mediaTypeList">-->
-                    <!--                    <option value="book" />-->
-                    <!--                    <option value="tv show" />-->
-                    <!--                    <option value="movie" />-->
-                    <!--                    <option value="video game" />-->
-                    <!--                    <option value="board game" />-->
-                    <!--                    <option value="artist" />-->
-                    <!--                    <option value="artist, music" />-->
-                    <!--                    <option value="theatre" />-->
-                    <!--                    <option value="food" />-->
-                    <!--                    <option value="article" />-->
-                    <!--                    <option value="author" />-->
+            <div class="flex flex-col space-y-2">
 
-                    <!--                </datalist>-->
+                <Label for="title">Title:</Label>
+                <Input name="title" label="Title:" value={form?.title}/>
 
-                    <RadioGroup.Root value="comfortable">
-                        <div class="flex items-center space-x-2">
-                            <RadioGroup.Item value="default" id="r1"/>
-                            <Label for="r1">Default</Label>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <RadioGroup.Item value="comfortable" id="r2"/>
-                            <Label for="r2">Comfortable</Label>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <RadioGroup.Item value="compact" id="r3"/>
-                            <Label for="r3">Compact</Label>
-                        </div>
-                        <RadioGroup.Input name="spacing"/>
-                    </RadioGroup.Root>
+                <Label for="media_type">Media type:</Label>
+                <input type="hidden" name="media_type" value={media_type}/>
+                <div>
+                    <Button
+                            onclick={() => media_type = 'book'}
+                            variant={media_type === 'book' ? 'secondary' : "outline" }
+                            aria-label="book">
+                        <Book/>
+                    </Button>
+                    <Button
+                            onclick={() => media_type = 'movie'}
+                            variant={media_type === 'movie' ? 'secondary' : "outline" }
+                            aria-label="movie">
+                        <Clapperboard/>
+                    </Button>
+                    <Button
+                            onclick={() => media_type = 'tv_show'}
+                            variant={media_type === 'tv_show' ? 'secondary' : "outline" }
+                            aria-label="tv show">
+                        <MonitorPlay/>
+                    </Button>
+                    <Button
+                            onclick={() => media_type = 'video_game'}
+                            variant={media_type === 'video_game' ? 'secondary' : "outline" }
+                            aria-label="video game">
+                        <Gamepad2/>
+                    </Button>
+                    <Button
+                            onclick={() => media_type = 'url'}
+                            variant={media_type === 'url' ? 'secondary' : "outline" }
+                            aria-label="web page">
+                        <Link/>
+                    </Button>
+                    <Button
+                            onclick={() => media_type = 'other'}
+                            variant={media_type === 'other' ? 'secondary' : "outline" }
+                            aria-label="other"><Input name="other_media_type" placeholder="other" class=""/>
+                    </Button>
+                </div>
 
+                <Label for="notes">Notes:</Label>
+                <Textarea name="notes" value={form?.title} placeholder="What do you think???"/>
 
-                    <input type="hidden" name="status" value={status}/>
-                    <div>
-                        <span onclick={() => status = RecommendationStatus.Interested}><ToggleButton color="yellow"
-                                                                                                     selected={status === RecommendationStatus.Interested}>&#10133; Interested</ToggleButton></span>
-                        <span onclick={() => status = RecommendationStatus.Watching}><ToggleButton color="orange"
-                                                                                                   selected={status === RecommendationStatus.Watching}>&#10133; Watching</ToggleButton></span>
-                        <span onclick={() => status = RecommendationStatus.Recommend}><ToggleButton color="blue"
-                                                                                                    selected={status === RecommendationStatus.Recommend}>&#10133; Recommend</ToggleButton></span>
-                    </div>
-
-                    <p>TODO: Should be text area</p>
-                    <Input name="notes" label="Notes:"/>
-                    <Card>
-
-                        <H1>Extras</H1>
-                        <Input name="author" label="Author/Artist/Creator:"/>
-                        <Input name="rating" label="Rating:" placeholder="1-5"/>
-                        <Input name="completed" label="Completed?" placeholder="yes/no"/>
-                        <Input name="who_recommended" label="Who recommended?:"/>
-                    </Card>
-
-
-                </Card>
+                <FormButton>Create recommendation</FormButton>
+                
             </div>
-            <FormButton>Create recommendation</FormButton>
         </form>
     </Card>
 

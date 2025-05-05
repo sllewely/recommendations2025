829ae1a3fe84f9d45fd92fa2576bf71d116dfb5e
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-05-04 21:28:28 -0400

    or create an account on sign in page

--- web/src/routes/(welcome_pages)/sign_in/+page.svelte
+++ web/src/routes/(welcome_pages)/sign_in/+page.svelte
@@ -4,9 +4,10 @@
     import Input from "$lib/components/form/Input.svelte";
     import FormButton from "$lib/components/form/FormButton.svelte";
     import H1 from "$lib/components/text/H1.svelte";
+    import Link from '$lib/components/text/Link.svelte'
 
-    import { current_user } from '$lib/state/current_user.svelte';
-    import { toasts, newToast, ToastType } from "$lib/state/toast.svelte";
+    import {current_user} from '$lib/state/current_user.svelte';
+    import {toasts, newToast, ToastType} from "$lib/state/toast.svelte";
 
 
     let {data, form} = $props();
@@ -17,16 +18,17 @@
 </script>
 <div>
 
-<H1>Sign in</H1>
+    <H1>Sign in</H1>
+    <Link url="/create_account">Or create an account</Link>
 
-{#if creating }
-    <p>signing in...</p>
-{/if}
+    {#if creating }
+        <p>signing in...</p>
+    {/if}
 
-<form
-        method="POST"
-        action="?/signin"
-        use:enhance={() => {
+    <form
+            method="POST"
+            action="?/signin"
+            use:enhance={() => {
             creating = true;
             return async ({update, result}) => {
                 await update();
@@ -46,13 +48,13 @@
 
         }}
 
->
+    >
 
-    <div class="flex flex-col">
-        <Input name="email" label="E-mail:" value={form?.email} required=true />
-        <Input name="password" label="Password:" input_type="password" required=true />
+        <div class="flex flex-col">
+            <Input name="email" label="E-mail:" value={form?.email} required=true/>
+            <Input name="password" label="Password:" input_type="password" required=true/>
 
-    </div>
-    <FormButton>Sign in</FormButton>
-</form>
+        </div>
+        <FormButton>Sign in</FormButton>
+    </form>
 </div>

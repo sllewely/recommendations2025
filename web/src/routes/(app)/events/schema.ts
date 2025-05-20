123fd7391e7e05200e87c5b0208b316853a3aff8
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-05-20 17:26:48 -0400

    work in progress time picker on events form

--- web/src/routes/(app)/events/schema.ts
+++ web/src/routes/(app)/events/schema.ts
@@ -2,6 +2,9 @@ import {z} from "zod";
 
 export const eventsFormSchema = z.object({
         title: z.string().min(2).max(120),
+        start_date: z.string(),
+        end_date: z.string().nullable(),
+        time_input: z.string(),
 
     }
 );

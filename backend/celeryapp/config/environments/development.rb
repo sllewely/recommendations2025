27bdbb69594fc294c8ddae241464b75ac980242a
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-05-21 14:20:01 -0400

    I sent an email to myself

--- backend/celeryapp/config/environments/development.rb
+++ backend/celeryapp/config/environments/development.rb
@@ -64,4 +64,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.action_mailer.delivery_method = :resend
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
---
+++ backend/celeryapp/spec/models/send_email_helper_test.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'json'
+include SendEmailHelper
+
+RSpec.describe "SendEmailHelper", type: :helper do
+  describe "send_test_email" do
+
+    before(:context) do
+      @my_user = create(:user, email: 'se.llewelyn@gmail.com')
+
+    end
+
+    it 'emails me' do
+      SendEmailHelper.send_test_email(@my_user)
+    end
+
+  end
+end

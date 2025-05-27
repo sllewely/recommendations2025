00749ea0c492e5962be8b1427ce572845ba78cd0
Mash <mashnoon33@gmail.com>
2025-05-27 11:52:16 -0400

    ignore

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { QueryClientProvider } from "@tanstack/svelte-query";
+	import type { LayoutData } from "./$types";
+	import { SvelteQueryDevtools } from "@tanstack/svelte-query-devtools";
+	export let data: LayoutData;
+</script>
+
+<QueryClientProvider client={data.queryClient}>
+	<slot />
+	<SvelteQueryDevtools />
+</QueryClientProvider>

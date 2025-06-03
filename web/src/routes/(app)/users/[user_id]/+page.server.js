e1947182574cdc62d548905f0cca537d567bfa69
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-06-03 14:38:35 -0400

    user profile page shows useful stuff

--- web/src/routes/(app)/users/[user_id]/+page.server.js
+++ web/src/routes/(app)/users/[user_id]/+page.server.js
@@ -7,12 +7,8 @@ export async function load({ cookies, params }) {
 
 	let user_id = params.user_id;
 	let user = await getUser(jwt, user_id);
-	let posts = await getPostsForUser(jwt, user_id);
-	let recommendations = await getRecommendationsForUser(jwt, user_id, { user_id: user_id });
 
 	return {
 		user: user["res"],
-		posts: posts,
-		recommendations: recommendations,
 	};
 }
--- web/src/routes/(app)/users/[user_id]/+page.svelte
+++ web/src/routes/(app)/users/[user_id]/+page.svelte
@@ -1,44 +1,32 @@
 <script lang="ts">
 	import H1 from "$lib/components/text/H1.svelte";
-	import FeedItem from "$lib/components/posts/FeedItem.svelte";
 	import H2 from "$lib/components/text/H2.svelte";
-	import ToggleButton from "$lib/components/form/ToggleButton.svelte";
-	import { RecommendationStatus } from "$lib/enums";
+	import { Badge } from "$lib/components/ui/badge";
+	import { friends_map } from "$lib/state/friends_map.svelte";
 
 	let { data } = $props();
 	let user = data.user;
-	let posts = data.posts;
-
-	let selected = $state("recommendations");
+	const tags = user.tags;
+	const is_friends = friends_map.friends_map[user.id];
 </script>
 
-<H1>{user.name}</H1>
-<p>Your friend, {user.name}, because presently everyone on the app is your friend</p>
-
 <div>
-	<span onclick={() => (selected = "activity")}
-		><ToggleButton selected={selected === "activity"}>Activity</ToggleButton></span
-	>
-	<span onclick={() => (selected = "recommendations")}
-		><ToggleButton selected={selected === "recommendations"}>Recommendations</ToggleButton></span
-	>
+	<div class="flex space-x-2 mb-4 items-baseline">
+		<H1>{user.name}</H1>
+		{#if tags}
+			<div class="space-x-2 mb-4">
+				{#each tags as tag}
+					<Badge>{tag}</Badge>
+				{/each}
+			</div>
+		{/if}
+	</div>
+	<div>
+		{#if is_friends}
+			<div>friends</div>
+		{:else}
+			<div>not friends</div>
+		{/if}
+	</div>
+	<div>{user.blurb}</div>
 </div>
-
-{#if selected === "activity"}
-	<H1>Posts</H1>
-	<div class="col-span-2 flex flex-col">
-		<H2>the feed</H2>
-		{#each data.posts as feed_item}
-			<FeedItem {feed_item} />
-		{/each}
-	</div>
-{/if}
-
-{#if selected === "recommendations"}
-	<H1>Recommendations</H1>
-	<div class="col-span-2 flex flex-col">
-		{#each data.recommendations as feed_item}
-			<FeedItem {feed_item} />
-		{/each}
-	</div>
-{/if}

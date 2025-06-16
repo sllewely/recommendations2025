c57f6b8591c449051d936c77980d4ad67c3a63a1
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-06-16 16:46:17 -0400

    update calendar and range calendar

--- web/src/lib/components/ui/calendar/calendar-header.svelte
+++ web/src/lib/components/ui/calendar/calendar-header.svelte
@@ -2,15 +2,18 @@
 	import { Calendar as CalendarPrimitive } from "bits-ui";
 	import { cn } from "$lib/utils.js";
 
-	type $$Props = CalendarPrimitive.HeaderProps;
-
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: CalendarPrimitive.HeaderProps = $props();
 </script>
 
 <CalendarPrimitive.Header
-	class={cn("relative flex w-full items-center justify-between pt-1", className)}
-	{...$$restProps}
->
-	<slot />
-</CalendarPrimitive.Header>
+	bind:ref
+	class={cn(
+		"h-(--cell-size) flex w-full items-center justify-center gap-1.5 text-sm font-medium",
+		className,
+	)}
+	{...restProps}
+/>

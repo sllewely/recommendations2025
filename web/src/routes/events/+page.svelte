4d16b0ae1dd1891312cccad928c78f59db0fbd98
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-10-28 12:24:53 -0400

    deprecate community events for now

--- web/src/routes/events/+page.svelte
+++ web/src/routes/events/+page.svelte
@@ -26,59 +26,63 @@
 		<H1>Community Events</H1>
 	</div>
 	<div>
-		<div class="grid grid-cols-3">
-			<div>
-				{#if creating}
-					<p>creating...</p>
-				{/if}
-				<form
-					method="POST"
-					action="?/create_event"
-					use:enhance={() => {
-						creating = true;
-						return async ({ update, result }) => {
-							await update({ reset: false });
-							creating = false;
-							let res = result.data;
-							if (res.success) {
-								console.log("success create event");
-								newToast("You have successfully created an event!!");
-								goto("/events");
-							} else {
-								newToast("Error creating an event: " + res.message, ToastType.Error);
-							}
-						};
-					}}
-				>
-					<div class="flex flex-col">
-						<Card>
-							<H1>Create an event</H1>
-							<p class="text-xs">You'll need to be logged in to make an event!</p>
-							<InputCheckbox name="public" label="Public event" value="true" />
+		<p>This page is for aggregating public events happening in your neighborhood</p>
+		<p>I don't have time to work on this feature. Let me know if you want to collaborate!</p>
+	</div>
+	<div>
+		<!--		<div class="grid grid-cols-3">-->
+		<!--			<div>-->
+		<!--				{#if creating}-->
+		<!--					<p>creating...</p>-->
+		<!--				{/if}-->
+		<!--				<form-->
+		<!--					method="POST"-->
+		<!--					action="?/create_event"-->
+		<!--					use:enhance={() => {-->
+		<!--						creating = true;-->
+		<!--						return async ({ update, result }) => {-->
+		<!--							await update({ reset: false });-->
+		<!--							creating = false;-->
+		<!--							let res = result.data;-->
+		<!--							if (res.success) {-->
+		<!--								console.log("success create event");-->
+		<!--								newToast("You have successfully created an event!!");-->
+		<!--								goto("/events");-->
+		<!--							} else {-->
+		<!--								newToast("Error creating an event: " + res.message, ToastType.Error);-->
+		<!--							}-->
+		<!--						};-->
+		<!--					}}-->
+		<!--				>-->
+		<!--					<div class="flex flex-col">-->
+		<!--						<Card>-->
+		<!--							<H1>Create an event</H1>-->
+		<!--							<p class="text-xs">You'll need to be logged in to make an event!</p>-->
+		<!--							<InputCheckbox name="public" label="Public event" value="true" />-->
 
-							<Input name="title" label="Title:" value={form?.title} />
-							<InputDateTime date_value={form?.date_input} time_value={form?.time_input} />
-							<!--                            <InputDateTime name="end" date_value={form?.date_input} time_value="{form?.time_input}" />-->
-							<Input name="description" label="Description:" />
-							<Input name="address" label="Address:" value={form?.address} />
-							<Input name="url" label="URL:" value={form?.url} />
-							<Input name="event_type" label="Event Type:" value={form?.event_type} />
-						</Card>
-					</div>
-					<FormButton>Create event</FormButton>
-				</form>
-			</div>
+		<!--							<Input name="title" label="Title:" value={form?.title} />-->
+		<!--							<InputDateTime date_value={form?.date_input} time_value={form?.time_input} />-->
+		<!--							&lt;!&ndash;                            <InputDateTime name="end" date_value={form?.date_input} time_value="{form?.time_input}" />&ndash;&gt;-->
+		<!--							<Input name="description" label="Description:" />-->
+		<!--							<Input name="address" label="Address:" value={form?.address} />-->
+		<!--							<Input name="url" label="URL:" value={form?.url} />-->
+		<!--							<Input name="event_type" label="Event Type:" value={form?.event_type} />-->
+		<!--						</Card>-->
+		<!--					</div>-->
+		<!--					<FormButton>Create event</FormButton>-->
+		<!--				</form>-->
+		<!--			</div>-->
 
-			<div class="col-span-2">
-				<H1>Events</H1>
-				{#each data.events as event_item}
-					{#if !!event_item["date_header"]}
-						<DateHeader {event_item} />
-					{:else}
-						<EventFeedItem {event_item} />
-					{/if}
-				{/each}
-			</div>
-		</div>
+		<!--			<div class="col-span-2">-->
+		<!--				<H1>Events</H1>-->
+		<!--				{#each data.events as event_item}-->
+		<!--					{#if !!event_item["date_header"]}-->
+		<!--						<DateHeader {event_item} />-->
+		<!--					{:else}-->
+		<!--						<EventFeedItem {event_item} />-->
+		<!--					{/if}-->
+		<!--				{/each}-->
+		<!--			</div>-->
+		<!--		</div>-->
 	</div>
 </div>

b2bf80338d69cc1d272e158c1227fd643c392a69
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-11-11 17:38:48 -0500

    notifications page

--- web/src/routes/settings/notifications/+page.svelte
+++ web/src/routes/settings/notifications/+page.svelte
@@ -2,6 +2,30 @@
 	import H1 from "$lib/components/text/H1.svelte";
 	import { Button } from "$lib/components/ui/button";
 	import EnableNotifications from "$lib/components/notifications/EnableNotifications.svelte";
+	import { onMount } from "svelte";
+	import { toast } from "svelte-sonner";
+	import { invalidateAll } from "$app/navigation";
+
+	let service_worker_found = $state(false);
+
+	onMount(async () => {
+		const registration = await navigator.serviceWorker.ready;
+		service_worker_found = registration.active !== null;
+	});
+
+	let click_unsubscribe = async () => {
+		console.log("click!");
+		const registration = await navigator.serviceWorker.ready;
+
+		let subscription = await registration.pushManager.getSubscription();
+		if (subscription) {
+			await subscription.unsubscribe();
+			toast.success("unsubscribed from notifications");
+		} else {
+			toast.error("dont have an active subscription");
+		}
+		toast.info("refresh the page to subscribe");
+	};
 </script>
 
 <div class="flex flex-col">
@@ -10,9 +34,16 @@
 		Web push notifications must be enabled on each device you want to receive notifications.
 	</div>
 	<div>
+		{#if service_worker_found}
+			Service worker found
+		{:else}
+			Service worker not found
+		{/if}
+	</div>
+	<div>
 		<EnableNotifications />
 	</div>
 	<div>
-		<Button>Unsubscribe</Button>
+		<Button onclick={click_unsubscribe}>Unsubscribe</Button>
 	</div>
 </div>

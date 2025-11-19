fac0ae6baa9f979cb923c817b9c8bd0f09d185b9
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-11-19 14:29:53 -0500

    blueprinter events

--- backend/celeryapp/app/controllers/events_controller.rb
+++ backend/celeryapp/app/controllers/events_controller.rb
@@ -1,18 +1,10 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate, only: [:public_events]
 
   def index
     @events = Event.includes(:user, rsvps: :user, comments: :user).where('start_date_time > ?', DateTime.now).order(start_date_time: :asc)
 
     # TODO: improve query performance
     render json: EventBlueprint.render(@events, view: :authed), status: :ok
-  end
-
-  def public_events
-    @events = Event.includes(:rsvps).where('start_date_time > ?', DateTime.now).order(start_date_time: :asc)
-
-    # TODO: improve query performance
-    render json: @events.map { |e| e.attributes.merge({ current_user_rsvp: e.rsvp_status_for_current_user(current_user), rsvps: e.rsvps }) }, status: :ok
   end
 
   def create
@@ -26,14 +18,14 @@ class EventsController < ApplicationController
     current_user.friends.each do |friend|
       PushNotification.send_push_notification(friend, "New Event", "#{current_user.name} posted a new event")
     end
-    render json: @event, status: :created
+    render json: EventBlueprint.render(@event, view: :authed), status: :created
   end
 
   def show
     @event = Event.includes(:comments).find_by(id: params[:id])
     # TODO: PERMISSIONS
     if @event
-      render json: EventBlueprint.render(@event, view: :authed, current_user: current_user), status: :ok
+      render json: EventBlueprint.render(@event, view: :authed), status: :ok
     else
       render json: @event.errors, status: :not_found
     end
@@ -41,12 +33,12 @@ class EventsController < ApplicationController
 
   def update
     # find will raise; find_by_id will return nil if the object not found
-    @event = current_user.events.find_by_id(params[:id])
+    @event = current_user.events.find_by(id: params[:id])
     if @event.nil?
       render json: { error: "event not found" }, status: :not_found and return
     end
     if @event.update(event_params)
-      render json: @event.attributes, status: :ok
+      render json: EventBlueprint.render(@event, view: :authed), status: :ok
     else
       render json: @event.errors, status: :unprocessable_content
     end
@@ -54,18 +46,18 @@ class EventsController < ApplicationController
   end
 
   def destroy
-    @event = current_user.events.find_by_id(params[:id])
+    @event = current_user.events.find_by(id: params[:id])
     if @event.nil?
       render json: { error: "event not found" }, status: :not_found and return
     end
     @event.destroy
-    render json: nil, status: :ok
+    render json: nil, status: :no_content
   end
 
   private
 
   def event_params
-    params.except(:id).permit(:title, :description, :start_date_time, :address, :url, :event_type, :end_date_time, :is_public)
+    params.except(:id).permit(:title, :description, :start_date_time, :address, :url, :event_type, :end_date_time)
   end
 
 end

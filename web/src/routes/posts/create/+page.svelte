399865d9d6977fcd4aafecc41c2df129539ec15c
Sarah Llewelyn <se.llewelyn@gmail.com>
2025-11-25 15:58:42 -0500

    debouncing the post

--- web/src/routes/posts/create/+page.svelte
+++ web/src/routes/posts/create/+page.svelte
@@ -5,7 +5,6 @@
 	import H1 from "$lib/components/text/H1.svelte";
 	import Card from "$lib/components/Card.svelte";
 	import { Textarea } from "$lib/components/ui/textarea/index.js";
-	import showdown from "showdown";
 
 	import { newToast, ToastType } from "$lib/state/toast.svelte.js";
 	import { goto } from "$app/navigation";
@@ -14,8 +13,17 @@
 
 	let creating = $state(false);
 
-	let converter = new showdown.Converter();
-	let rendered = $derived(form?.content);
+	let rendered = $state(form?.content);
+	let test = $state("");
+
+	let timer: number;
+	const debounce = (v: string) => {
+		console.log(v);
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			test = v;
+		}, 750);
+	};
 </script>
 
 <div>
@@ -48,7 +56,11 @@
 					<div class="flex flex-col pb-4">
 						<Input name="title" label="Title:" value={form?.title} />
 
-						<Textarea name="content" value={form?.content} />
+						<Textarea
+							name="content"
+							bind:value={rendered}
+							on:keyup={({ target: { value } }) => debounce(value)}
+						/>
 					</div>
 					<FormButton>Create post</FormButton>
 				</form>
@@ -58,6 +70,7 @@
 			<Card>
 				{rendered}
 			</Card>
+			<Card>{test}</Card>
 		</div>
 	</div>
 </div>

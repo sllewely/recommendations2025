ddf7c2a7812da8d53d47d28e0c3599ff60c5b09d
Sarah Llewelyn <se.llewelyn@gmail.com>
2026-01-06 12:50:19 -0500

    more tests for deleting a user with events and recommendations

--- backend/celeryapp/spec/models/user_spec.rb
+++ backend/celeryapp/spec/models/user_spec.rb
@@ -2,11 +2,37 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "#destroy" do
-    it "deletes the user's feed items when the user is destroyed" do
+    it "deletes the user's post feed items when the user is destroyed" do
       user = create(:user)
       # Create a feedable object (e.g., a Post) for the feed item
       post = create(:post, user: user)
       feed_item = post.feed_item
+
+      expect {
+        user.destroy
+      }.to change(FeedItem, :count).by(-1)
+
+      expect(FeedItem.exists?(feed_item.id)).to be_falsey
+    end
+
+    it "deletes the user's event feed items when the user is destroyed" do
+      user = create(:user)
+      # Create a feedable object (e.g., a Post) for the feed item
+      event = create(:event, user: user)
+      feed_item = event.feed_item
+
+      expect {
+        user.destroy
+      }.to change(FeedItem, :count).by(-1)
+
+      expect(FeedItem.exists?(feed_item.id)).to be_falsey
+    end
+
+    it "deletes the user's recommendation feed items when the user is destroyed" do
+      user = create(:user)
+      # Create a feedable object (e.g., a Post) for the feed item
+      recommendation = create(:recommendation, user: user)
+      feed_item = recommendation.feed_item
 
       expect {
         user.destroy

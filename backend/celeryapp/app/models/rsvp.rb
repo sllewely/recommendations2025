e9103608b5a1dfe570a1bf54404a1d45f70c8e88
Sarah Llewelyn <se.llewelyn@gmail.com>
2026-01-21 15:44:47 -0500

    scope for viewing all events I have access to

--- backend/celeryapp/app/models/rsvp.rb
+++ backend/celeryapp/app/models/rsvp.rb
@@ -2,6 +2,6 @@ class Rsvp < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  enum :status, [:interested, :cant_go, :going, :not_interested]
+  enum :status, [:interested, :cant_go, :going, :not_interested, :invited]
 
 end

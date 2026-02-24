04f3a191132dac7eb96b71c11bdb0a2f0aeecf0d
Sarah Llewelyn <se.llewelyn@gmail.com>
2026-02-24 18:08:47 -0500

    icalendar gem exports something

---
+++ backend/celeryapp/app/controllers/calendars_controller.rb
@@ -0,0 +1,30 @@
+require 'icalendar'
+
+class CalendarsController < ApplicationController
+
+  skip_before_action :authenticate
+
+  def index
+    # need api key for user
+    # get events going into the past by a month
+    @events = Event
+                .month_past_onward
+                .order(start_date_time: :asc)
+
+    cal = Icalendar::Calendar.new
+    cal.event do |e|
+      e.dtstart = Icalendar::Values::Date.new('20050428')
+      e.dtend = Icalendar::Values::Date.new('20050429')
+      e.summary = "Meeting with the man."
+      e.description = "Have a long lunch meeting and decide nothing..."
+      e.ip_class = "PRIVATE"
+    end
+
+    # unknown magic
+    cal.publish
+
+    render plain: cal.to_ical, status: :ok
+
+  end
+
+end
---
+++ backend/celeryapp/spec/requests/calendars_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'json'
+
+RSpec.describe "Calendars", type: :request do
+  describe 'index' do
+
+    before(:all) do
+      @my_user = create(:user)
+
+      headers = { 'ACCEPT' => 'application/json' }
+      post "/sign_in", params: { email: @my_user.email, password: @my_user.password }, headers: headers
+
+      auth_token = JSON.parse(response.body)["auth_token"]
+      @headers = { 'ACCEPT' => 'application/json', 'Authorization' => "Token #{auth_token}" }
+    end
+
+    it 'returns ics of events' do
+
+      get "/calendars"
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to start_with('BEGIN:VCALENDAR')
+      # expect(response.body).to include()
+      expect(response.body).to include('END:VCALENDAR')
+    end
+
+  end
+end

8f2e86e32df3073d1cabd433727e4a5bdfb060d0
Sarah Llewelyn <se.llewelyn@gmail.com>
2026-04-02 13:02:46 -0400

    collapsible grid group members

--- web/src/routes/groups/[id]/+page.svelte
+++ web/src/routes/groups/[id]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { Group } from "$lib/api_calls/types";
 	import UserCard from "$lib/components/users/UserCard.svelte";
+	import * as Collapsible from "$lib/components/ui/collapsible";
+	import Link from "$lib/components/text/Link.svelte";
 
 	interface Props {
 		data: {
@@ -12,14 +14,22 @@
 </script>
 
 <div>
-	<h1>{data.group.name}</h1>
+	<h1 class="text-2xl font-bold">{data.group.name}</h1>
 
-	<h2>Members</h2>
-	<div>
-		{#each data.group.users as user}
-			<UserCard {user} />
-		{/each}
-	</div>
+	<Collapsible.Root>
+		<Collapsible.Trigger>
+			<Link>
+				<h2 class="text-xl font-bold">{data.group.users.length} Members...</h2>
+			</Link>
+		</Collapsible.Trigger>
+		<Collapsible.Content>
+			<div class="grid grid-cols-2 lg:grid-cols-4 gap-4">
+				{#each data.group.users as user}
+					<UserCard {user} />
+				{/each}
+			</div>
+		</Collapsible.Content>
+	</Collapsible.Root>
 
 	<h2>Posts</h2>
 </div>

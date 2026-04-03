41f75527656e1ed80ad5a8fc364936d43b0826ba
Sarah Llewelyn <se.llewelyn@gmail.com>
2026-04-03 12:52:32 -0400

    dark mode for posts

--- web/src/lib/components/posts/PostCard.svelte
+++ web/src/lib/components/posts/PostCard.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
-	import Card from "$lib/components/Card.svelte";
-	import Link from "$lib/components/text/Link.svelte";
-	import H2 from "$lib/components/text/H2.svelte";
-	import PlusCircle from "$lib/components/posts/PlusCircle.svelte";
-	import { current_user } from "$lib/state/current_user.svelte";
+	import * as Card from "$lib/components/ui/card/index.js";
 	import type { Post } from "$lib/api_calls/types";
-	import { parseAbsoluteToLocal } from "@internationalized/date";
 	import MarkedDownPost from "$lib/components/posts/MarkedDownPost.svelte";
 
 	interface Props {
@@ -24,12 +19,21 @@
 	<div>
 		<div class="p-2">
 			<a href="/posts/{feed_item.id}">
-				<Card border_color="border-orange-500" hover_color="hover:bg-orange-100">
-					{#if feed_item.title}<H2>{feed_item.title}</H2>{/if}
-					{#if feed_item.content}
-						<MarkedDownPost captured_text={feed_item.content} />
+				<Card.Root
+					class="border-orange-500 hover:bg-orange-100 dark:hover:bg-orange-900 dark:bg-gray-900 dark:text-gray-200"
+				>
+					{#if feed_item.title}
+						<Card.Header>
+							<Card.Title>{feed_item.title}</Card.Title>
+						</Card.Header>
 					{/if}
-				</Card>
+
+					<Card.Content>
+						{#if feed_item.content}
+							<MarkedDownPost captured_text={feed_item.content} />
+						{/if}
+					</Card.Content>
+				</Card.Root>
 			</a>
 		</div>
 	</div>
